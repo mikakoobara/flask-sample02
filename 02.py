fd64b9ba46ed9e395619a661db97c723960cf97a
obaramikako <quartersilver@gmail.com>
2018-12-01 16:26:04 +0900

    おみくじをランダムに表示

--- 02.py
+++ 02.py
@@ -1,3 +1,5 @@
+import random
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -10,6 +12,13 @@ def index():
 @app.route("/user/<name>")
 def hello(name):
     return render_template("hello.html", name=name)
+
+@app.route("/omikuji")
+def omikuji():
+    lists = ["大吉", "中吉", "小吉"]
+    result = random.choice(lists)
+    return render_template("omikuji.html", result=result)
+
 
 
 if __name__ == "__main__":
