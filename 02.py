177efd06f91e2dc60e3240a61867379d9b21016b
obaramikako <quartersilver@gmail.com>
2018-12-01 16:01:26 +0900

    hello.html追加

--- 02.py
+++ 02.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/user/<name>")
+def hello(name):
+    return render_template("hello.html", name=name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
